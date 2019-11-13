47695adb7e395675f1e0a87e79fe94e3b7d2ec2c
Joshua Holloway <joshua.holloway@disa.com>
2019-11-13 17:11:46 -0600

    progress, class work, etc

--- auto_mate.py
+++ auto_mate.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import json
+import threading
 import pyautogui
 import contextlib
 import pynput.mouse as ms
@@ -24,7 +25,7 @@ _record = 1
 # _record = 1 | on (Both)
 # _record = 2 | on (Mouse only)
 # _record = 3 | on (Keyboard only)
-action_items = []
+# ^(Soft-set.. Should turn on both Listeners and only act on actions if _record set)
 
 #class omni_listener():
 
@@ -78,7 +79,7 @@ def on_press(key):
                     _keyboard_buffer = _keyboard_buffer[:-1]
                 else: # [Otherwise add as special character for replay?]:
                     _act = action('key', {'x': _click_int_x, 'y': _click_int_y}, key)
-                    action_items.append(_act)
+                    stage.append(_act)
 
             # [Add space for spacebar xD]:
             if key == kb.Key.space:
@@ -133,7 +134,7 @@ def on_press(key):
             if _pass_thru:
                 check_keyboard_buffer(interrupt=True)
                 _act = action('key', {'x': _click_int_x, 'y': _click_int_y}, key)
-                action_items.append(_act)
+                stage.append(_act)
 
             CHECK_KEYBOARD_EMERGENCY(_hold_CTRL, _hold_SHIFT)
 
@@ -163,7 +164,7 @@ def check_keyboard_buffer(interrupt=False):
         if _keyboard_buffer != '':
             # [Action for Keyboard takes x,y of last click and keyboard_buffer]:
             _act = action('keyboard', {'x': _click_int_x, 'y': _click_int_y}, _keyboard_buffer)
-            action_items.append(_act)
+            stage.append(_act)
 
         _keyboard_buffer=''
         _typed_last=0
@@ -174,7 +175,7 @@ def on_click(x, y, button, pressed):
     _int_y = int(y)
 
     if _record > 0:
-        check_keyboard_buffer()
+        check_keyboard_buffer(interrupt=True)
 
         if pressed:
             _click_int_x = int(x)
@@ -190,7 +191,7 @@ def on_click(x, y, button, pressed):
                 _CMD_input = True
                 _pass = input('Enter password here: ')
                 _act = action('pass', {'x': _click_int_x, 'y': _click_int_y}, _pass)
-                action_items.append(_act)
+                stage.append(_act)
 
                 print('Click field to resume! / Please use Submit instead of Enter')
                 return
@@ -198,10 +199,10 @@ def on_click(x, y, button, pressed):
             # [SAME = POS | DIFF = BOX]:
             if abs(_int_x-_click_int_x) < 5 and abs(_int_y-_click_int_y) < 5:
                 _act = action('click', [{'x': _int_x, 'y': _int_y}])
-                action_items.append(_act)
+                stage.append(_act)
             else:
                 _act = action('box', [{'x': _click_int_x, 'y': _click_int_y}, {'x': _int_x, 'y': _int_y}])
-                action_items.append(_act)
+                stage.append(_act)
 
 def on_move(x, y):
     global _record
@@ -235,12 +236,10 @@ def timer(msg):
     print('%s: %.02fms'%(msg, (end-start)*1000))
 
 
-class action():
+class action:
     _id = 0
     _state = None
     _action_id = 0
-    _kb_ctrl = None
-    _ms_ctrl = None
     _coords_list = None
     _keyboard_buffer = None
 
@@ -250,55 +249,101 @@ class action():
         self._action_id = action._id
         self._coords_list = coords_list
         self._keyboard_buffer = keyboard_buffer
-        self._kb_ctrl = kb.Controller()
-        self._ms_ctrl = ms.Controller()
 
         # [If coords weren't sent as list, add as list]:
         if coords_list and type(coords_list)!=list:
             self._coords_list = [coords_list]
 
-        self.print_info('Added')
+        self._print('Added')
 
         # [Class ID incremetor]:
         action._id+=1
 
-    def replay(self):
-        self.print_info('Replay')
+    # [Serializer]:
+    def _JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    # [Pretty Print actions]:
+    def _print(self, act):
+        for _coord in self._coords_list:
+            if self._keyboard_buffer is None or self._state == 'pass':
+                print('{0} action{1}({2}): {3}'.format(act, self._action_id, self._state, _coord))
+            else:
+                print('{0} action{1}({2}): {3} | {4}'.format(act, self._action_id, self._state, _coord, self._keyboard_buffer))
+
+
+# [Iterator class]:
+class action_iterator:
+    _index = None
+    _stage = None
+
+    def __init__(self, stage):
+        self._index = 0
+        self._stage = stage
+
+    def __next__(self):
+        if self._index < len(self._stage._action_items): # Check if action_items are fully iterated or not
+            result = self._stage._action_items[self._index]
+
+            self._index +=1
+            return result
+
+        raise StopIteration
+
+# [Stage Manager to organize actions]:
+class stage_manager:
+    _kb_ctrl = None
+    _ms_ctrl = None
+    _action_items = None
+
+    def __init__(self):
+        self._kb_ctrl = kb.Controller()
+        self._ms_ctrl = ms.Controller()
+        self._action_items = []
+
+    def __iter__(self):
+        return action_iterator(self)
+
+    def append(self, action):
+        self._action_items.append(action)
+
+    def _replay(self, action):
+        action._print('Replay')
 
         # [CLICK| Click Position]: 
         # ^(Need to check click/double-click for replay)
-        if self._state == 'click':
-            _x = self._coords_list[0].get('x')
-            _y = self._coords_list[0].get('y')
+        if action._state == 'click':
+            _x = action._coords_list[0].get('x')
+            _y = action._coords_list[0].get('y')
             pyautogui.moveTo(_x, _y, duration=1)
             self._ms_ctrl.click(Button.left, 1) # pyautogui wont change active window (OSX)
 
         # [KEYBOARD| replay typing]:
-        if self._state == 'keyboard':
-            pyautogui.typewrite(self._keyboard_buffer, interval=.1)
+        if action._state == 'keyboard':
+            pyautogui.typewrite(action._keyboard_buffer, interval=.1)
 
         # [KEY| Special Keys]:
-        if self._state == 'key':
-            self._kb_ctrl.press(self._keyboard_buffer)
-            self._kb_ctrl.release(self._keyboard_buffer)
+        if action._state == 'key':
+            self._kb_ctrl.press(action._keyboard_buffer)
+            self._kb_ctrl.release(action._keyboard_buffer)
             time.sleep(.1)
 
         # [PASS| Enter password]:
-        if self._state == 'pass':
+        if action._state == 'pass':
             # [Move mouse && click on _x, _y]: (?)
-            _x = self._coords_list[0].get('x')
-            _y = self._coords_list[0].get('y')
+            _x = action._coords_list[0].get('x')
+            _y = action._coords_list[0].get('y')
             pyautogui.click(x=_x, y=_y, button='left', clicks=1)
 
-            pyautogui.typewrite(self._keyboard_buffer, interval=.1)
+            pyautogui.typewrite(action._keyboard_buffer, interval=.1)
 
         # [BOX| SSIM?]:
-        if self._state == 'box':
+        if action._state == 'box':
             print('SSIM?')
-            _start_x = self._coords_list[0].get('x')
-            _start_y = self._coords_list[0].get('y')
-            _stop_x = self._coords_list[1].get('x')
-            _stop_y = self._coords_list[1].get('y')
+            _start_x = action._coords_list[0].get('x')
+            _start_y = action._coords_list[0].get('y')
+            _stop_x = action._coords_list[1].get('x')
+            _stop_y = action._coords_list[1].get('y')
             _diff_x = (_stop_x - _start_x)
             _diff_y = (_stop_y - _start_y)
 
@@ -312,56 +357,41 @@ class action():
         # [1sec pause]:
         time.sleep(1)
 
-    # [Pretty Print actions]:
-    def print_info(self, act):
-        for _coord in self._coords_list:
-            if self._keyboard_buffer is None or self._state == 'pass':
-                print('{0} action{1}({2}): {3}'.format(act, self._action_id, self._state, _coord))
-            else:
-                print('{0} action{1}({2}): {3} | {4}'.format(act, self._action_id, self._state, _coord, self._keyboard_buffer))
 
-
-# [0]: (Merge listeners into 1 class / Merge controllers into 1 class)
-# [1]: Need a way so save python class objects (actions)
-# [2]: MIRROR actions across screen / can set up same page on left/right screen, record on left, and replay on right.
-# [3]: (use BOX coords for SSIM checking)
+# [0]: Need a way so save python class objects (actions)
+# [1]: (Merge listeners into 1 class / Merge controllers into 1 class)
+# [2]: (use BOX coords for SSIM checking)
+# [3]: MIRROR actions across screen / can set up same page on left/right screen, record on left, and replay on right.
 # https://pyautogui.readthedocs.io/en/latest/cheatsheet.html
 # https://pythonhosted.org/pynput/mouse.html#controlling-the-mouse
 # https://pythonhosted.org/pynput/keyboard.html#controlling-the-keyboard
 if __name__ == "__main__":
-    # [Record Actions]:
-    if _record > 0:
-        if _record == 1 or _record == 2:
-            # [non-blocking mouse listener]:
-            mouse_listener = ms.Listener(on_click=on_click, on_move=on_move)
-            mouse_listener.start()
+    # [GLOBALS]:
+    stage = stage_manager()
+    mouse_listener = ms.Listener(on_click=on_click, on_move=on_move)
+    keyboard_listener = kb.Listener(on_press=on_press, on_release=on_release)
+    # ^(non-blocking mouse/keyboard listener)
 
-        if _record == 1 or _record == 3:
-            # [non-blocking keyboard listener]:
-            keyboard_listener = kb.Listener(on_press=on_press, on_release=on_release)
-            keyboard_listener.start()
-
-        print('[Mouse listening for clicks! Press CTRL to stop.]')
+    with mouse_listener, keyboard_listener:
+        print('[Mouse/Keyboard listening! Press CTRL to stop recording]')
         while _record > 0:
-            time.sleep(1)
-        print('[Exiting listening loop!]')
+            time.sleep(1) #pass
 
-    #-------
-    print('[Starting Replay]: 3sec')
-    time.sleep(3)
-    #-------
+        #-------
+        print('[Starting Replay]: 3sec')
+        time.sleep(3)
+        #-------
 
-    # [Replay Actions]:
-    if _record == 0:
-        _again = True
-        while _again:
-            for _action in action_items:
-                _action.replay()
+         # [Replay Actions]:
+        if _record == 0:
+            _again = True
+            while _again:
+                for action in stage:
+                    stage._replay(action)
 
-            _again = input('[Do you wish to Replay again?]:')
-            _again = False if (_again.lower() == 'n' or _again.lower() == 'no') else True
+                #_again = False # Turn off Replay Again
+                if _again:
+                    _again = input('[Do you wish to Replay again?]:')
+                    _again = False if (_again.lower() == 'n' or _again.lower() == 'no') else True
 
-    print('[fin.]')
-    mouse_listener.stop()
-    keyboard_listener.stop()
-
+        print('[fin.]')
